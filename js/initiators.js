04824eafb4707d81d90383e8eb0ef699c1aa8537
thunderdunk <dan@heptagoncreative.com>
2014-08-28 16:14:47 -0700

    Setup sass compiling, js/magnific popup, jquery ui links, etc

---
+++ js/initiators.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function() {
+	$('.test-popup-link').magnificPopup({ 
+	  type: 'image'
+		// other options
+	});
+});
---
+++ js/min/initiators-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".test-popup-link").magnificPopup({type:"image"})});
