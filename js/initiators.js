107df17cc98cffde0e6c0d59fb8350e4bd3d23e3
thunderdunk <dan@heptagoncreative.com>
2014-09-08 17:21:26 -0700

    Started grid layout, masthead setup, Font Awesome, slider setup

--- js/initiators.js
+++ js/initiators.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 	
-	// Can also be used with $(document).ready()
+	
 	  $('.flexslider').flexslider({
 	    animation: "slide"
 	  });
@@ -22,10 +22,24 @@ $(document).ready(function() {
 	});
 
 
+	
+
 	//Form test
 	$('.open-popup-link').magnificPopup({
 	  type:'inline',
 	  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
 	});
-});
+}); //end document ready
 
+//Header change to solid when scroll below slier
+$(window).scroll(function() {
+	var scroll = $(window).scrollTop();
+
+	//Reveal Top link 
+	if (scroll >= 300) {
+		$("#masthead").addClass("solid");
+	} else {
+		$("#masthead").removeClass("solid");
+	}
+
+}); //end window scrol
