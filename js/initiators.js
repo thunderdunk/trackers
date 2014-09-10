68bfd275ae72b47947025f6d2b4a1f3892a11a37
thunderdunk <dan@heptagoncreative.com>
2014-09-09 17:01:48 -0700

    Removed MMenu, as usual, replaced with custom off-canvas nav area

--- js/initiators.js
+++ js/initiators.js
@@ -2,18 +2,36 @@
 
 $(document).ready(function() {
 	
+
 	
+
+	/* Off Canvas nav */
+	$(".open-panel").click(function(){
+  
+		$("html").toggleClass("openNav");
+		//$('#page').toggleClass('open');
+	});
+	
+	//if ($('#page').hasClass('open')) {
+		//Click anywhere in primary content area to close menu
+		$('main').click(function() {
+			$('html').removeClass('openNav');
+			//$('#page').removeClass('open');
+		});
+	//}
+	
+
+
 	  $('.flexslider').flexslider({
 	    animation: "slide"
 	  });
 
-	 // $( ".accordion" ).accordion();
-
-	  $('.accordion .head').click(function() {
-	  		$(this).toggleClass('active');
-			$(this).next().toggle('medium');
-			return false;
-	  }).next().hide();
+	//Homemade accordion - opens multiple items at once
+	$('.accordion .head').click(function() {
+			$(this).toggleClass('active');
+		$(this).next().toggle('medium');
+		return false;
+	}).next().hide();
 
 	$( ".tabs" ).tabs();
 
@@ -22,16 +40,19 @@ $(document).ready(function() {
 		// other options
 	});
 
-	//MMenu
-	 $("#main-navigation").mmenu({
-         // options
-      }, {
-         // configuration
-         offCanvas: {
-            pageNodetype: "section"
-         }
+/*	//MMenu
+	 $("#mobile-navigation").mmenu({
+        // options
+        slidingSubmenus: false,
+        labels: true,
+        offCanvas: {
+        	position: 'right',
+        }
       });
+	*/
+
 	
+
 
 	//Form test
 	$('.open-popup-link').magnificPopup({
@@ -39,6 +60,8 @@ $(document).ready(function() {
 	  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
 	});
 }); //end document ready
+
+
 
 //Header change to solid when scroll below slier
 $(window).scroll(function() {
